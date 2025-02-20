4e0c9902f854e2f216573180f05655151792e2c3
Pakin49 <philgus884@gmail.com>
2025-02-20 20:33:46 +0700

    Done Ex6

--- src/main/java/Ex6_6580043/Exercise_Ex6.java
+++ src/main/java/Ex6_6580043/Exercise_Ex6.java
@@ -13,21 +13,29 @@ class OneCard {
         this.score = sc;
         // represent card suit by %4 result
         // 0 = club, 1 = diamonds, 2 = hearts, 3 = spades
-        this.suit = sc % 4;
+        this.suit = sc / 13;
         // jack = 11, queen = 12, king =13
-        this.rank = (sc/4)+1;
+        this.rank = (sc%13)+1;
     }
 
     //Setter & Getter
-    void set_score(int score){ this.score  = score;}
     int get_score(){ return this.score;}
-    int get_suit(){ return this.suit;}
-    int rank(){ return this.rank;}
-
-    // methods
-    String print_card(){
-        String s_rank = "";
-        if(rank)
+    String get_suit(){
+        String s = "";
+        if(this.suit==0){ s = "Clubs";}
+        else if(this.suit==1){ s = "Diamonds";}
+        else if(this.suit==2){ s = "Hearts";}
+        else { s = "Spades";}
+        return s;
+    }
+    String get_rank(){
+        String r = "";
+        if (this.rank == 1){r = "A";}
+        else if (this.rank == 11){ r = "J";}
+        else if (this.rank == 12){ r = "Q";}
+        else if (this.rank == 13){ r = "K";}
+        else { r = String.valueOf(this.rank);}
+        return r;
     }
 }
 
@@ -38,25 +46,51 @@ class CardThread extends Thread {
     private final int tid; // thread_id use for printing
 
     // Constructors
-    public CardThread (int thread_num){
-        this.tid  = thread_num;
-
-    }
+    public CardThread (int thread_num){ this.tid  = thread_num; randomCards = new ArrayList<>(); this.start();}
 
     //methods
-    void print_cards(int round){
-        out.printf("Round %3d  [ ",round);
+    boolean check_rank(){
+        String suit = randomCards.get(0).get_rank();
+        boolean result = true;
+        for(int i=1; i < 4; i++){
+            if(!suit.equals(randomCards.get(i).get_rank())){
+                result = false;
+            }
+        }
+        return result;
     }
-    void draw_cards(int number_card){
-        for (int i = 0; i < number_card; i++) {
-            Random random = new Random();
+    boolean check_suit(){
+        String suit = randomCards.get(0).get_suit();
+        boolean result = true;
+        for(int i=1; i < 4; i++){
+            if(!suit.equals(randomCards.get(i).get_suit())){
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    void print_cards(int round){
+        out.printf("Round %3d  [",round);
+        for(int i = 0; i < 4; i++){
+            out.printf(" %2s of %-8s ",randomCards.get(i).get_rank(),randomCards.get(i).get_suit());
+            if(i<3){
+                out.printf(",");
+            }
+        }
+        out.println("]\n");
+    }
+    void draw_cards(){
+        Random random = new Random();
+        for (int i = 0; i < 4; i++) {
             int rand_int = random.nextInt(52); // random integer
 
             // Detect if card is duplicated?
             boolean duplicate_card = false;
-            for (int j = 0; j < randomCards.size(); j++) {
-                if (randomCards.get(j).get_score() == rand_int) {
+            for (OneCard randomCard : randomCards) {
+                if (randomCard.get_score() == rand_int) {
                     duplicate_card = true;
+                    break;
                 }
             }
             if (duplicate_card) {
@@ -71,36 +105,52 @@ class CardThread extends Thread {
     @Override
     public void run() {
 
-        String directory = "src/main/java/";
-        String path = directory+this.tid;
-        File out_file = new File(path);
-
-        int round = 0;
-        boolean finish = false;
-        while(!finish) {
-            randomCards.removeAll(randomCards);
-            draw_cards(4); // draw 4 cards
-            print_cards(round);//print the card in the ArrayList
-
-
-            // Create PrintWriter object to write result to a separate file
-            // Execute steps 1-3 in loop:
-            // 1. Draw 4 cards from the same deck. The cards must not duplicate.
-            // 2. Print round number and these 4 cards to output file.
-            // 3. If all cards are from the same suit or have equal rank, stop the loop.
-            // Otherwise, clear randomCards & continue to the next round.
-            // After the loop, print #rounds to the screen
+        String directory = "src/main/java/Ex6_6580043/T";
+        String path = directory+this.tid+".txt";
+        try {
+            File out_file = new File(path);
+            out = new PrintWriter(out_file);
+        }catch (Exception e) {
+            System.err.println(e.getClass().getName());
         }
-
+            int round = 0;
+            boolean finish = false;
+            while (!finish) {
+                draw_cards(); // draw 4 cards
+                print_cards(round);//print the card in the ArrayList
+                round += 1;
+                if (check_suit() || check_rank()) {
+                    finish = true;
+                }
+                randomCards.clear();
+            }
+            out.close();
+            System.out.printf("Thread T%d finishes in %3d rounds\n", this.tid, round);
     }
 }
 
 public class Exercise_Ex6 {
+    static ArrayList<CardThread> thread_list = new ArrayList<>();
+
     public static void main(String[] args) {
-        CardThread T0 = new CardThread(0); T0.start();
-        CardThread T1 = new CardThread(1); T1.start();
-        CardThread T2 = new CardThread(2); T2.start();
+        // Scan for numbers of threads
+        Scanner key_scan = new Scanner(System.in);
+        System.out.println("Number of threads = ");
+        int num_threads = 0;
 
+        // If the input is not valid ask for another int
+        while (true) {
+            try {
+                num_threads = key_scan.nextInt();
+                break;
+            } catch (RuntimeException re) {
+
+                System.err.println("Enter valid integer");
+                key_scan.nextLine();
+            }
+        }
+        for (int i = 0; i < num_threads; i++) {
+            thread_list.add(new CardThread(i));
+        }
     }
-
 }
